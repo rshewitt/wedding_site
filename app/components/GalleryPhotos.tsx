e6ca74df12517da0c0d358628df0d88541515ea3
Reid Hewitt <sam.hew.r@gmail.com>
2024-03-07 16:50:19 -0700

    update photos

--- app/components/GalleryPhotos.tsx
+++ app/components/GalleryPhotos.tsx
@@ -1,26 +1,26 @@
 import { useEffect, useState } from "react";
-import foto1 from "~/images/foto1.jpg";
-import foto2 from "~/images/foto2.jpg";
-import foto3 from "~/images/foto3.jpg";
-import foto4 from "~/images/foto4.jpg";
-import foto5 from "~/images/foto5.jpg";
-import foto6 from "~/images/foto6.jpg";
-import foto7 from "~/images/foto7.jpg";
-import foto8 from "~/images/foto8.jpg";
-import foto9 from "~/images/foto9.jpg";
+import foto1 from "~/images/DSC_1879.jpg";
+import foto2 from "~/images/DSC_1891.jpg";
+import foto3 from "~/images/DSC_1955.jpg";
+import foto4 from "~/images/DSC_1965.jpg";
+import foto5 from "~/images/DSC_1989.jpg";
+import foto6 from "~/images/DSC_1993.jpg";
+// import foto7 from "~/images/foto7.jpg";
+// import foto8 from "~/images/foto8.jpg";
+// import foto9 from "~/images/foto9.jpg";
 import Gallery from "./Gallery";
 import type { IPhoto } from "./Gallery";
 
 const photos: IPhoto[] = [
   {
     src: foto1,
-    width: 3,
+    width: 10,
     height: 2,
     alt: "pic 1",
   },
   {
     src: foto3,
-    width: 4,
+    width: 15,
     height: 5,
     alt: "pic 2",
   },
@@ -32,40 +32,40 @@ const photos: IPhoto[] = [
   },
   {
     src: foto6,
-    width: 1,
+    width: 5,
     height: 1,
     alt: "pic 4",
   },
   {
     src: foto5,
-    width: 3,
+    width: 8,
     height: 2,
     alt: "pic 5",
   },
-  {
-    src: foto4,
-    width: 4,
-    height: 5,
-    alt: "pic 6",
-  },
-  {
-    src: foto9,
-    width: 5,
-    height: 4,
-    alt: "pic 7",
-  },
-  {
-    src: foto8,
-    width: 3,
-    height: 2,
-    alt: "pic 8",
-  },
-  {
-    src: foto7,
-    width: 2,
-    height: 3,
-    alt: "pic 9",
-  },
+//   {
+//     src: foto4,
+//     width: 4,
+//     height: 5,
+//     alt: "pic 6",
+//   },
+//   {
+//     src: foto9,
+//     width: 5,
+//     height: 4,
+//     alt: "pic 7",
+//   },
+//   {
+//     src: foto8,
+//     width: 3,
+//     height: 2,
+//     alt: "pic 8",
+//   },
+//   {
+//     src: foto7,
+//     width: 2,
+//     height: 3,
+//     alt: "pic 9",
+//   },
 ];
 
 let isHydrating = true;
@@ -81,7 +81,7 @@ const GalleryPhotos = () => {
   return (
     <div className="w-full px-1 md:px-4">
       <h4 className="text-3xl font-head font-bold text-center mb-14 pt-10 text-gray-700">
-        Galeri
+        Gallery
       </h4>
       {isHydrated ? (
         <div className="w-full overflow-auto">

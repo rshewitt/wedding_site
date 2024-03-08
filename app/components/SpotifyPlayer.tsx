dc4fd86261489d6aed1b03d6b0af84a092513f43
Reid Hewitt <sam.hew.r@gmail.com>
2024-03-07 19:37:22 -0700

    add text content and styling

--- app/components/SpotifyPlayer.tsx
+++ app/components/SpotifyPlayer.tsx
@@ -11,14 +11,17 @@ const SpotifyPlaylist = () => {
   
   return (
     <div>
-      <div className="text-3xl font-head font-bold text-center mb-10 md:mb-16 text-gray-700 px-4">
+      <div className="text-3xl font-head font-bold text-center mb-4 md:mb-16 text-gray-700 px-4">
+        What song reminds you of us?
+        </div>
+        <div className="text-base font-head font-bold text-center mb-10 md:mb-16 text-gray-700 px-4">
         <a 
           // TODO: update this link
           href="https://open.spotify.com/playlist/1accWGTIiTGHdkpJhei0Ff?si=uHRxgcTkQC6ShCEox4ewOg&pt=12afd4560a6b1715fcf57ba7ab4780e9&pi=u-kTdBwbCRRl-s"
           target="_blank"
           rel="noreferrer"
           >
-          CLICK HERE TO ADD A SONG
+          Contribute to our Spotify playlist. Click HERE
         </a>
         </div>
         <div className="max-w-lg mx-auto px-4 text-center">
@@ -28,7 +31,6 @@ const SpotifyPlaylist = () => {
           height="352" 
           allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
         </div>
-      
       </div>
     );
   };

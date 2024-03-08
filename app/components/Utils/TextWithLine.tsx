affefcb35fa673ba9c3340b008c2f25a9bf74e44
Reid Hewitt <sam.hew.r@gmail.com>
2024-03-07 19:37:33 -0700

    change text size

--- app/components/Utils/TextWithLine.tsx
+++ app/components/Utils/TextWithLine.tsx
@@ -8,7 +8,7 @@ const TextWithLine = ({ children }: TextWithLineProps) => {
   return (
     <div className="flex items-center justify-center">
       <div className="border-t border-gray-400 w-8" />
-      <div className="text-center font-medium font-head text-gray-600 italic mx-3">
+      <div className="text-center font-medium text-lg font-head text-gray-600 italic mx-3">
         {children}
       </div>
       <div className="border-t border-gray-400 w-8" />

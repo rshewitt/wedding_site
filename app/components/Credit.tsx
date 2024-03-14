b8b901f384b1cd1310bc1be2fed70170934ba291
Reid Hewitt <sam.hew.r@gmail.com>
2024-03-13 19:12:30 -0600

    isolate credit section as component.

---
+++ app/components/Credit.tsx
@@ -0,0 +1,15 @@
+const Credit = () => {
+    const thisYear = new Date().getFullYear();
+    return (
+        <div className="py-24 bg-background text-gray-700 relative text-center font-sans">
+        © {thisYear} forked from 
+            <a 
+            className="font-semibold" 
+            href="https://github.com/fauzialz/amifauzi.com-open" 
+            target={"_blank"} 
+            rel="noreferrer"> Fauzi</a>
+      </div>
+    )
+}
+
+export default Credit;

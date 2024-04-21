8416200602fc2cbf3369050eff508d80ed8d5f3c
Reid Hewitt <sam.hew.r@gmail.com>
2024-04-21 15:28:09 -0600

    add custom font families and sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,18 @@ module.exports = {
                     "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
             },
         },
+        fontFamily: {
+            squiggly: ["var(--font-breezeblocks)"],
+            normal: ["var(--font-monstserrat)"],
+            normalBold: ["var(--font-monstserratBold)"],
+            normalItalic: ["var(--font-monstserratItalic)"],
+        },
+        fontSize: {
+            "md-twl": "0.85rem",
+            "xl-wel": "1.1rem",
+            "3xl-wel": "5rem",
+            "5xl-wel": "7rem",
+        },
     },
     plugins: [require("@tailwindcss/forms")],
 };

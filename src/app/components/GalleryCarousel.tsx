8b1d606922d2a4ac63ad306f943f6ff2ad39f1f5
Reid Hewitt <sam.hew.r@gmail.com>
2024-05-04 18:51:00 -0600

    update images

--- src/app/components/GalleryCarousel.tsx
+++ src/app/components/GalleryCarousel.tsx
@@ -8,9 +8,7 @@ import photo3 from "../images/DSC_1849.jpg";
 import photo4 from "../images/DSC_1912.jpg";
 import photo5 from "../images/DSC_1954.jpg";
 import photo6 from "../images/DSC_1931.jpg";
-import photo7 from "../images/DSC_1937.jpg";
 import photo8 from "../images/DSC_1946.jpg";
-import photo9 from "../images/DSC_1954.jpg";
 import photo10 from "../images/DSC_2005.jpg";
 import photo11 from "../images/IMG_5773.jpg";
 import photo12 from "../images/IMG_5776.jpg";
@@ -22,9 +20,7 @@ const photos = [
     photo4,
     photo5,
     photo6,
-    photo7,
     photo8,
-    photo9,
     photo10,
     photo11,
     photo12,

b7ae361796c194d681f9e0429f56be0448a2d3a2
Reid Hewitt <sam.hew.r@gmail.com>
2024-06-30 14:41:20 -0600

    fix apos

--- src/app/components/BridalParty.tsx
+++ src/app/components/BridalParty.tsx
@@ -4,92 +4,84 @@ import Laynnae from "../images/lynnae.jpg";
 import Profile, { ProfileMinor } from "./Utils/Profile";
 
 const peopleMain = [
-    {
-        pic: Maddy,
-        altVal: "maid_of_honor",
-        name: "Madelyn Hayes",
-        role: "Maid of Honor",
-        desc: "Brittany and Maddy met in Sacramento, CA in 2017. From friends, to roommates, to best friends, and beyond – Maddy and Brittany are practically sisters. They may live in different states now, but that doesn&apos;t stop them from getting together as much as humanly possible, usually over a game of pool and a few hazy IPAs.",
-    },
-    {
-        pic: Laynnae,
-        altVal: "matron_of_honor",
-        name: "Lynnae Petricevic",
-        role: "Matron of Honor",
-        desc: "Brittany and Lynnae met in Sacramento, CA in 2014. Lynnae was Brittany&apos;s first friend in California, a longtime career mentor, next-door neighbor of over 7 years, and a part of the family forever. Lynnae is a founding member of the Finer Things Club™ alongside her husband, Pero, and Brittany. Whether it&apos;s in the US or abroad, you&apos;re bound to see the two of them together.",
-    },
-    {
-        pic: Dima,
-        altVal: "best_man",
-        name: "Dmitriy Gumenyuk",
-        role: "Best Man",
-        desc: "Reid and Dima met at Mira Loma High School in Sacramento, CA in 2006. Over 18 years they have enjoyed countless conversations, memories, and beer. Let&apos;s be honest, a LOT of beer.",
-    },
+  {
+    pic: Maddy,
+    altVal: "maid_of_honor",
+    name: "Madelyn Hayes",
+    role: "Maid of Honor",
+    desc: "Brittany and Maddy met in Sacramento, CA in 2017. From friends, to roommates, to best friends, and beyond – Maddy and Brittany are practically sisters. They may live in different states now, but that doesn't stop them from getting together as much as humanly possible, usually over a game of pool and a few hazy IPAs.",
+  },
+  {
+    pic: Laynnae,
+    altVal: "matron_of_honor",
+    name: "Lynnae Petricevic",
+    role: "Matron of Honor",
+    desc: "Brittany and Lynnae met in Sacramento, CA in 2014. Lynnae was Brittany's first friend in California, a longtime career mentor, next-door neighbor of over 7 years, and a part of the family forever. Lynnae is a founding member of the Finer Things Club™ alongside her husband, Pero, and Brittany. Whether it's in the US or abroad, you're bound to see the two of them together.",
+  },
+  {
+    pic: Dima,
+    altVal: "best_man",
+    name: "Dmitriy Gumenyuk",
+    role: "Best Man",
+    desc: "Reid and Dima met at Mira Loma High School in Sacramento, CA in 2006. Over 18 years they have enjoyed countless conversations, memories, and beer. Let's be honest, a LOT of beer.",
+  },
 ];
 
 const peopleMinorBride = [
-    {
-        name: "Christina Zechmeister",
-        role: "Bridesmaid",
-    },
-    {
-        name: "Laura Barnick",
-        role: "Bridesmaid",
-    },
+  {
+    name: "Christina Zechmeister",
+    role: "Bridesmaid",
+  },
+  {
+    name: "Laura Barnick",
+    role: "Bridesmaid",
+  },
 ];
 
 const peopleMinorGroom = [
-    {
-        name: "Azeo Plescia",
-        role: "Groomsman",
-    },
-    {
-        name: "Ryan Thalken",
-        role: "Groomsman",
-    },
+  {
+    name: "Azeo Plescia",
+    role: "Groomsman",
+  },
+  {
+    name: "Ryan Thalken",
+    role: "Groomsman",
+  },
 ];
 
 const BridalParty = () => {
-    return (
-        <div id="bridal_party_div" className="pt-10 pb-4">
-            <div className="text-3xl-wel lg:text-5xl-wel font-squiggly text-center pt-10 text-gray-700 px-2">
-                <h4 className="mb-1">Bridal Party</h4>
-            </div>
-            <div className="w-full flex flex-wrap justify-center pb-10">
-                {peopleMain.map((person, idx) => (
-                    <Profile
-                        pic={person.pic}
-                        altVal={person.altVal}
-                        name={person.name}
-                        role={person.role}
-                        desc={person.desc}
-                        key={idx}
-                    />
-                ))}
-            </div>
-            <div className="max-w-3xl mx-auto text-center">
-                <div className="grid grid-cols-1 gap-4 text-center text-2xl px-4 py-8 md:px-0 md:text-3xl font-black">
-                    {peopleMinorBride.map((person, idx) => (
-                        <ProfileMinor
-                            name={person.name}
-                            role={person.role}
-                            key={idx}
-                        />
-                    ))}
-                </div>
-                <div className="text-xl text-black">~ ~</div>
-                <div className="grid grid-cols-1 gap-4 text-center text-2xl px-4 py-8 md:py-10 md:px-0 md:text-3xl font-black">
-                    {peopleMinorGroom.map((person, idx) => (
-                        <ProfileMinor
-                            name={person.name}
-                            role={person.role}
-                            key={idx}
-                        />
-                    ))}
-                </div>
-            </div>
+  return (
+    <div id="bridal_party_div" className="pt-10 pb-4">
+      <div className="text-3xl-wel lg:text-5xl-wel font-squiggly text-center pt-10 text-gray-700 px-2">
+        <h4 className="mb-1">Bridal Party</h4>
+      </div>
+      <div className="w-full flex flex-wrap justify-center pb-10">
+        {peopleMain.map((person, idx) => (
+          <Profile
+            pic={person.pic}
+            altVal={person.altVal}
+            name={person.name}
+            role={person.role}
+            desc={person.desc}
+            key={idx}
+          />
+        ))}
+      </div>
+      <div className="max-w-3xl mx-auto text-center">
+        <div className="grid grid-cols-1 gap-4 text-center text-2xl px-4 py-8 md:px-0 md:text-3xl font-black">
+          {peopleMinorBride.map((person, idx) => (
+            <ProfileMinor name={person.name} role={person.role} key={idx} />
+          ))}
         </div>
-    );
+        <div className="text-xl text-black">~ ~</div>
+        <div className="grid grid-cols-1 gap-4 text-center text-2xl px-4 py-8 md:py-10 md:px-0 md:text-3xl font-black">
+          {peopleMinorGroom.map((person, idx) => (
+            <ProfileMinor name={person.name} role={person.role} key={idx} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default BridalParty;
--- src/app/components/ThingsToDo.tsx
+++ src/app/components/ThingsToDo.tsx
@@ -1,55 +1,55 @@
 import Activity from "./Utils/Activity";
 
 const Activities = [
-    {
-        title: "Mardi Gras",
-        url: "https://www.mardigrasneworleans.com/parades/",
-        desc: "The biggest weekend to celebrate Mardi Gras in New Orleans is the weekend before our wedding, and we invite you to join us! If you&apos;re interested in attending Mardi Gras (even if it&apos;s just for one day!) please RSVP, and we&apos;ll send you additional information on our local celebration.",
-    },
-    {
-        title: "The French Quarter",
-        url: "https://www.frenchquarter.com/",
-        desc: "The French Quarter, also known as the Vieux Carré, is the oldest neighborhood in the city of New Orleans. Famous for its vibrant nightlife and colorful buildings with cast-iron balconies - there is plenty to see, shop, and eat here!",
-    },
-    {
-        title: "Haunted History Tours",
-        url: "https://hauntedhistorytours.com/",
-        desc: "New Orleans is a city like no other — featuring hundreds of years of history steeped in mystique. Take a stroll through the French Quarter on a guided tour to learn more about the city&apos;s spooky side.",
-    },
-    {
-        title: "Live Music",
-        url: "https://www.neworleans.com/events/live-music-calendar/",
-        desc: "Catch a concert while you&apos;re in town! From Preservation Hall to Tipitina&apos;s, there is something for everyone.",
-    },
-    {
-        title: "New Orleans City Park",
-        url: "https://neworleanscitypark.org/",
-        desc: "The Park is home to the New Orleans Botanical Garden, Couturie Forest and Arboretum, the New Orleans Museum of Art, the Louisiana Children&apos;s Museum, and the largest grove of mature live oaks in the world, some of which are nearly 800 years old.",
-    },
-    {
-        title: "Museums, Nature, and More",
-        url: "https://www.neworleans.com/blog/post/new-orleans-top-attractions/",
-        desc: "Whether you want to experience rich history, or a white alligator at the Audubon Zoo, this list is for you!",
-    },
+  {
+    title: "Mardi Gras",
+    url: "https://www.mardigrasneworleans.com/parades/",
+    desc: "The biggest weekend to celebrate Mardi Gras in New Orleans is the weekend before our wedding, and we invite you to join us! If you're interested in attending Mardi Gras (even if it's just for one day!) please RSVP, and we'll send you additional information on our local celebration.",
+  },
+  {
+    title: "The French Quarter",
+    url: "https://www.frenchquarter.com/",
+    desc: "The French Quarter, also known as the Vieux Carré, is the oldest neighborhood in the city of New Orleans. Famous for its vibrant nightlife and colorful buildings with cast-iron balconies - there is plenty to see, shop, and eat here!",
+  },
+  {
+    title: "Haunted History Tours",
+    url: "https://hauntedhistorytours.com/",
+    desc: "New Orleans is a city like no other — featuring hundreds of years of history steeped in mystique. Take a stroll through the French Quarter on a guided tour to learn more about the city's spooky side.",
+  },
+  {
+    title: "Live Music",
+    url: "https://www.neworleans.com/events/live-music-calendar/",
+    desc: "Catch a concert while you're in town! From Preservation Hall to Tipitina's, there is something for everyone.",
+  },
+  {
+    title: "New Orleans City Park",
+    url: "https://neworleanscitypark.org/",
+    desc: "The Park is home to the New Orleans Botanical Garden, Couturie Forest and Arboretum, the New Orleans Museum of Art, the Louisiana Children's Museum, and the largest grove of mature live oaks in the world, some of which are nearly 800 years old.",
+  },
+  {
+    title: "Museums, Nature, and More",
+    url: "https://www.neworleans.com/blog/post/new-orleans-top-attractions/",
+    desc: "Whether you want to experience rich history, or a white alligator at the Audubon Zoo, this list is for you!",
+  },
 ];
 
 const ThingsToDo = () => {
-    return (
-        <div id="ttd_dev" className="max-w-3xl mx-auto text-center">
-            <div className="text-3xl-wel lg:text-5xl-wel font-squiggly text-center mb-10 pt-10 text-gray-700 px-2">
-                <h4 className="mb-1">Things To Do</h4>
-            </div>
+  return (
+    <div id="ttd_dev" className="max-w-3xl mx-auto text-center">
+      <div className="text-3xl-wel lg:text-5xl-wel font-squiggly text-center mb-10 pt-10 text-gray-700 px-2">
+        <h4 className="mb-1">Things To Do</h4>
+      </div>
 
-            {Activities.map((activity, idx) => (
-                <Activity
-                    title={activity.title}
-                    url={activity.url}
-                    desc={activity.desc}
-                    key={idx}
-                />
-            ))}
-        </div>
-    );
+      {Activities.map((activity, idx) => (
+        <Activity
+          title={activity.title}
+          url={activity.url}
+          desc={activity.desc}
+          key={idx}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default ThingsToDo;
